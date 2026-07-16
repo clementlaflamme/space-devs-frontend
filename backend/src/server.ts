1668ed90bb85aa6f6fd8fc4bfbb6e1b2c8ce4e46
Clement Laflamme <clement_laflamme@videotron.ca>
2026-07-16 09:40:19 -0400

    ajout cors et installations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,22 @@
-import express, { type Request, type Response } from "express"
-import dotenv from "dotenv"
-import authRoutes from "./routes/auth.routes.js"
-import lancementRoutes from "./routes/lancement.routes.js"
-dotenv.config() 
+import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import lancementRoutes from "./routes/lancement.routes.js";
+import cors from "cors";
+dotenv.config();
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-    res.json({message: "LaunchPad - Mission Control "})
-})
+  res.json({ message: "LaunchPad - Mission Control " });
+});
 
+app.use(cors());
 // localhost:3000/auth/....
-app.use("/auth",authRoutes)
+app.use("/auth", authRoutes);
 
-app.use("/lancement",lancementRoutes)
+app.use("/lancement", lancementRoutes);
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Serveur sur http://localhost:${PORT}`))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Serveur sur http://localhost:${PORT}`));
