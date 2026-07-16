e52b93856a7cd3d36bccc4d7e6dd6ba15e77bc0f
powemetal <francisbvrt@gmail.com>
2026-07-16 11:42:45 -0400

    [Feat] ajouter un decompte

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,9 +23,33 @@ function App() {
 
   const [listeLancements, setListeLancements] = useState<Lancement[]>([]);
   const [msgErreur, setMsgErreur] = useState("");
+  const [limite, setLimite] = useState("");
 
-  async function recupererListeLancements() {
-    let reponse = await back.post("/lancement/importer", { limite: 10 });
+  const [maintenant, setMaintenant] = useState(Date.now())
+  useEffect(() => {
+    const interval = setInterval(()=>{
+      setMaintenant(Date.now())}, 1000)
+  })
+
+  function calculerDecompte(dateLancement: string) {
+    const maintenant = new Date().getTime()
+    const lancement = new Date(dateLancement).getTime()
+
+    const diff = lancement - maintenant
+    if (diff <= 0) return "Lancement effectué"
+
+    const jours = Math.floor(diff / (1000 * 60 * 60 * 24))
+    const heures = Math.floor((diff / (1000 * 60 * 60)) % 24)
+    const minutes = Math.floor((diff / (1000 * 60)) % 60)
+    const secondes = Math.floor((diff / 1000) % 60)
+
+  return `${jours}j ${heures}h ${minutes}m ${secondes}s`
+}
+
+
+
+  async function recupererListeLancements(limiteLancements: string) {
+    let reponse = await back.post("/lancement/importer", { limite: limiteLancements });
     if (reponse.status !== 201) {
       setMsgErreur("Erreur lors de la récupération des lancements de l'API.");
       console.error(reponse.data.error);
@@ -56,12 +80,16 @@ function App() {
   }
 
   useEffect(() => {
-    recupererListeLancements();
+    recupererListeLancements("10");
   }, []);
 
   return (
     <div>
       <h1>Launchpad</h1>
+      <div>
+        <button onClick={() => recupererListeLancements(limite)}>Importer</button>
+        <input id='inputLimite' placeholder="# lancements a importer" onChange={(e) => setLimite(e.target.value)}></input>
+      </div>
       <h2>Lancements</h2>
       <h2>Liste de Lancements ({listeLancements.length})</h2>
       <p>{msgErreur}</p>
@@ -123,7 +151,7 @@ function App() {
                       flexGrow: "1",
                     }}
                   >
-                    DECOMPTE
+                    {calculerDecompte(l.dateLancement)}
                   </div>
                 </div>
               </div>
